a042d6a175c147faf3f41ec3fd2e4226c4d66d93
Maxence Bouret <maxence.bouret@gmail.com>
2018-08-29 11:44:19 +0200

    Add of room new

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router  = express.Router();
+const Room = require("../models/Room");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -11,7 +12,19 @@ router.get('/rooms/new', (req, res, next) => {
 });
 
 router.post('/rooms/new', (req, res, next) => {
-  res.render('rooms-new');
+  Room.create({
+    name: req.body.name,
+    description : req.body.description
+  }).then(newRoom => {
+    res.redirect("/rooms/"+ newRoom._id)
+  })
+});
+
+router.get('/rooms/:id', (req, res, next) => {
+  Room.findById(req.params.id)
+  .then(room => {
+    res.render('room-detail', { room });
+  })
 });
 
 module.exports = router;
