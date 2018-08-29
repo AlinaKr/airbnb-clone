9c1c32e5d5dd5c369a0147759bd1f725994623c4
Maxence Bouret <maxence.bouret@gmail.com>
2018-08-29 11:05:58 +0200

    Add of models

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
   username: String,
-  password: String
+  password: String,
+  role: {type:String, enum: ['ADMIN', 'CUSTOMER'], default: 'CUSTOMER'}
 }, {
   timestamps: {
     createdAt: 'created_at',
