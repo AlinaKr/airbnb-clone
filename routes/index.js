5f1714497dc2315cb92ed8f4714d4f8a9ee5f880
Maxence Bouret <maxence.bouret@gmail.com>
2018-08-29 11:21:36 +0200

    Add of GET /rooms/new

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/rooms/new', (req, res, next) => {
+  res.render('rooms-new');
+});
+
+router.post('/rooms/new', (req, res, next) => {
+  res.render('rooms-new');
+});
+
 module.exports = router;
